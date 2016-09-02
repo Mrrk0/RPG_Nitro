9853f38353e18afd0dbb87f6a1315d83d9a2ab10
Mrrk0 <mrrk0@hotmail.com>
2016-09-02 21:36:30 +0300

    Test
    Works?

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
     // Can inherit monobehaviour
-
+    /*- inherits from MonoBehaviour
+- creates other Managers
+- hold all static information
+- holds Inventory
+- creates visual prefabs based on info from other Managers*/
 
 
 	// Use this for initialization
